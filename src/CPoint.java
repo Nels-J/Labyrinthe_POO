381c06f2d186056c79540a3208b6b61d581ba3d7
NelsJ <jesus.nels@gmail.com>
2022-07-10 20:00:19 +0200

    feat: override methods added for display + for equality object comparison

--- src/CPoint.java
+++ src/CPoint.java
@@ -17,10 +17,40 @@ public class CPoint {
 
 
     // ------------------ METHOD(s) ------------------ //
+    public void display(){
+        System.out.println("Point" +this);
+    }
+   public void display(String comment){
+        System.out.println(comment + this);
+    }
 
     @Override
     public String
     toString() {
         return "("+ x +","+y +')';
     }
+
+    // Overriding equals() to compare two CPoint objects
+    @Override
+    public boolean equals(Object o) {
+
+        // If the object is compared with itself then return true
+        if (o == this) {
+            return true;
+        }
+
+        /* Check if o is an instance of CPoint or not
+          "null instanceof [type]" also returns false */
+        if (!(o instanceof CPoint)) {
+            return false;
+        }
+
+        // typecast o to CPoint so that we can compare data members
+        CPoint c = (CPoint) o;
+
+        // Compare the data members and return accordingly
+        return Integer.compare(x, c.x) == 0
+                && Integer.compare(y, c.y) == 0;
+    }
+
 }
